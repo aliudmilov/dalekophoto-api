ca7f6ca80999ca3acb3ff4c73792d54d7a4262bf
Aleksandar Aleksandrov <aliudmilov@yahoo.com>
2022-03-20 22:27:38 +0000

    Add the right middleware order

--- DalekoPhoto.Api/Program.cs
+++ DalekoPhoto.Api/Program.cs
@@ -8,15 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IAlbumRepository, FileSystemAlbumRepository>();
 builder.Services.AddSingleton<IMemoryCache, MemoryCache>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
 });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -25,14 +24,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseDeveloperExceptionPage();
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles(new StaticFileOptions
 {
@@ -47,7 +44,6 @@ app.UseStaticFiles(new StaticFileOptions
              "Cache-Control", $"public, max-age={(60 * 60 * 24 * 7).ToString()}");
     }
 });
-
 app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
